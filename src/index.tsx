45ebbd749a5b59e882f11fc7d62ed4428976d357
Candice Guitton <candiceguitton@mbp-de-candice.home>
2025-05-15 15:57:18 +0200

    fix route

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL ||"/soundcheck-origination-form"}>
         <Routes>
           <Route path="/" element={<App />} />
           
