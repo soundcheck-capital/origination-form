2195750ca908130b1efb1f24a6813589caefcbc0
Candice Guitton <candiceguitton@mac.home>
2025-07-30 17:51:38 +0200

    redirects

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appElement = screen.getByRole('main');
+  expect(appElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Provider, useSelector } from 'react-redux';
-import { RootState, store } from './store';
 import './App.css';
-import MultiStepForm from './components/MultiStepForm';
-import ProtectedRoute from './components/ProtectedRoute';
-import SubmitSuccess from './components/SubmitSuccess';
 
 function App() {
-  const isSubmitted = useSelector((state: RootState) => state.form.isSubmitted);
   return (
-    <Provider store={store}>
-      <Router>
-        {isSubmitted ? <div className="App">
-          <Routes>
-            {/* Route par défaut - redirige vers le formulaire protégé */}
-
-            {/* Route du formulaire protégée par mot de passe */}
-            <Route
-              path="/form"
-              element={
-                <ProtectedRoute>
-                  <MultiStepForm />
-                  <Route path="/" element={<Navigate to="/form" replace />} />
-
-                  {/* Route de succès après soumission */}
-                  <Route path="/submit-success" element={<SubmitSuccess />} />
-
-                  {/* Route de fallback */}
-                  <Route path="*" element={<Navigate to="/form" replace />} />
-                </ProtectedRoute>
-              }
-            />
-
-          </Routes>
-        </div> : <SubmitSuccess />
-      }
-      </Router>
-    </Provider>
+    <div className="App" role="main">
+      {/* App.tsx ne gère plus le routing, c'est fait dans index.tsx */}
+    </div>
   );
 }
 
